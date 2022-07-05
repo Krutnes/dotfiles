c7c20db76441ea670030af6e5c89c6c71fa16227
Krutnes <andreas.krutnes@gmail.com>
2022-07-05 22:55:13 +0200

    Removed zsh and p10k config files, added alias appender instead

--- setup.sh
+++ setup.sh
@@ -3,13 +3,4 @@
 # @TODO:
 # Create install script with ZSH+plugins
 
-
-dir=$(pwd)
-
-rm -f -- $HOME/.bash_aliases
-rm -f -- $HOME/.p10k.zsh
-rm -f -- $HOME/.zshrc
-
-ln -s $dir/.zshrc $HOME/.zshrc
-ln -s $dir/.bash_aliases $HOME/.bash_aliases
-ln -s $dir/.p10k.zsh $HOME/.p10k.zsh
+echo $(pwd)/.zsh_aliases >> $HOME/.zshrc
